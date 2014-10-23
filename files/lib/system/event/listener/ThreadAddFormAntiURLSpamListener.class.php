2d66139c5a35329421f3fb8f70f754089a998ca5
Oliver Schlöbe <scripts@schloebe.de>
2014-10-23 13:02:43 +0200

    1.1.0.1

--- files/lib/system/event/listener/ThreadAddFormAntiURLSpamListener.class.php
+++ files/lib/system/event/listener/ThreadAddFormAntiURLSpamListener.class.php
@@ -17,64 +17,68 @@ use wcf\system\WCF;
 class ThreadAddFormAntiURLSpamListener implements IEventListener {
 	private $illegalChars = '[^\x0-\x2C\x2E\x2F\x3A-\x40\x5B-\x60\x7B-\x7F]+';
 	private $sourceCodeRegEx = null;
-
-	public $text = '';
-
+	
 	/**
-	 * url count in text
+	 * holds the event object on execution
+	 *
+	 * @var object
+	 */
+	protected $obj = null;
+	
+	/**
+	 * post/thread message
+	 *
+	 * @var string
+	 */
+	public $text = '';
+	
+	/**
+	 * external url count in text
 	 *
 	 * @var integer
 	 */
 	protected $urlCount = 0;
-
+	
 	/**
 	 * image count in text
 	 *
 	 * @var integer
 	 */
 	protected $imgCount = 0;
-
-	/**
-	 * Stores whether the disabled flasg was set.
-	 */
-	protected $disabledWasSet = false;
 	
 	/**
-	 * @see	\wcf\system\event\IEventListener::execute()
-	 */
-	protected $obj = null;
-
-	/**
-	 * @see }wcf\system\event\IEventListener::execute()
+	 * @see \wcf\system\event\IEventListener::execute()
 	 */
 	public function execute($obj, $className, $eventName) {		
 		$controller = $_GET['controller'];
 		$returnValues = $obj->getReturnValues();
 		$actionName = $obj->getActionName();
 		
-		if( in_array($className, array('wbb\data\post\ThreadAction', 'wbb\data\post\PostAction')) ) {
-			switch( $actionName ) {
-				case 'triggerPublication':
-				case 'update':
-					$objects = $obj->getObjects();
-					if( empty($objects[0]) ) return;
-					
-					$data = $objects[0];
-						
-					// check all disablers
-					if ( $data->isDisabled || !POST_LINKRESTRICTION_ENABLE
-						|| WCF::getSession()->getPermission('user.board.canBypassLinkRestriction')
-						|| WCF::getUser()->wbbPosts > POST_LINKRESTRICTION_MIN_POSTS ) return;
-						
-					// get parsed text
-					$text = $this->parse( $data->getMessage() );
-						
-					if( ($this->urlCount > POST_LINKRESTRICTION_MAX_URLS)
-						|| (POST_LINKRESTRICTION_ENABLE_IMAGE_RESTRICTION && $this->imgCount > POST_LINKRESTRICTION_MAX_IMAGES) ) {
-						$obj->disable();
-					}
-					break;
-			}
+		switch( $actionName ) {
+			case 'triggerPublication':
+			case 'update':
+				$objects = $obj->getObjects();
+				if (empty($objects[0])) {
+					return;
+				}
+				
+				$data = $objects[0];
+				
+				// check all disablers
+				if ($data->isDisabled || !POST_LINKRESTRICTION_ENABLE
+					|| WCF::getSession()->getPermission('user.board.canBypassLinkRestriction')
+					|| WCF::getUser()->wbbPosts > POST_LINKRESTRICTION_MIN_POSTS) {
+					return;
+				}
+				
+				// get parsed text
+				$text = $this->parse($data->getMessage());
+				
+				if (($this->urlCount > POST_LINKRESTRICTION_MAX_URLS)
+					|| (POST_LINKRESTRICTION_ENABLE_IMAGE_RESTRICTION && $this->imgCount > POST_LINKRESTRICTION_MAX_IMAGES)) {
+					$obj->disable();
+				}
+				break;
 		}
 	}
 
@@ -108,28 +112,27 @@ class ThreadAddFormAntiURLSpamListener implements IEventListener {
 
 		// add url tags
 		$this->text = preg_replace($urlPattern, '[url]\\0[/url]', $this->text);
-
+		
 		// search in text w/o code bbcodes for urls
 		preg_match_all('~\[url(.*)\](.*)\[\/url\]~isU', $this->text, $matches, PREG_SET_ORDER);
 		$count = 0;
 		foreach ($matches as $match) {
-			$match1 = trim($match[0], '"=\'');
-			$match2 = trim($match[1], '"=\'');
-			$match3 = trim($match[2], '"=\'');
-			if( !\wcf\system\application\ApplicationHandler::getInstance()->isInternalURL($match2) &&
-				!\wcf\system\application\ApplicationHandler::getInstance()->isInternalURL($match3)
-			) $count++;
+			$match1 = trim($match[1], '"=\'');
+			$match2 = trim($match[2], '"=\'');
+			if (!\wcf\system\application\ApplicationHandler::getInstance()->isInternalURL($match1) &&
+				!\wcf\system\application\ApplicationHandler::getInstance()->isInternalURL($match1)) {
+				$count++;
+			}
 		}
 		$this->urlCount = $count;
-
-		if( POST_LINKRESTRICTION_ENABLE_IMAGE_RESTRICTION ) {
+		
+		if (POST_LINKRESTRICTION_ENABLE_IMAGE_RESTRICTION) {
 			// search in text for img bbcodes
 			// quite primitve at the moment
 			preg_match_all('~\[img(.*)\](.*)(\[\/img\])?~isU', $this->text, $matches, PREG_SET_ORDER);
 			$this->imgCount = count($matches);
 		}
-
+		
 		return $this->text;
 	}
 }
-?>
